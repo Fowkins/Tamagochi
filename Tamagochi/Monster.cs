3e87f5c4d4fe380f8a74d0cc48d0d2142d374949
Черников Андрей <chernikov_1999@mail.ru>
2024-03-12 14:32:47 +0700

    Добавьте файлы проекта.

---
+++ Tamagochi/Monster.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagochi
+{
+    class Monster
+    {
+        private string name;
+        private int health;
+        private int hunger;
+        private int fatigue;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if(value > 10)
+                {
+                    health = 10;
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
+
+        public int Hunger
+        {
+            get
+            {
+                return hunger;
+            }
+            set
+            {
+                if(value > 10)
+                {
+                    hunger = 10;
+                }
+                else
+                {
+                    hunger = value;
+                }
+            }
+        }
+
+        public int Fatigue
+        {
+            get
+            {
+                return fatigue;
+            }
+            set
+            {
+                if (value > 10)
+                {
+                    fatigue = 10;
+                }
+                else
+                {
+                    fatigue = value;
+                }
+            }
+        }
+        public Monster(string name)
+        {
+            Name = name;
+            Health = 10;
+            Hunger = 0;
+            Fatigue = 0;
+        }
+
+        public void Feed()
+        {
+            if (Hunger > 0)
+            {
+                Hunger--;
+            }
+            else
+            {
+                Health--;
+            }
+        }
+
+        public void Play()
+        {
+            Fatigue++;
+            if (Fatigue == 10)
+            {
+                Health--;
+                Hunger++;
+            }
+
+        }
+
+        public void Sleep()
+        {
+            Fatigue = 0;
+            Health++;
+            Hunger++;
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Hunger: {Hunger}");
+            Console.WriteLine($"Fatigue: {Fatigue}");
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ Tamagochi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Tamagochi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tamagotchi!");
+            Console.Write("Enter pet name: ");
+            string petName = Console.ReadLine();
+            Monster monster = new Monster(petName);
+            bool gameOver = false;
+
+            while (!gameOver)
+            {
+                monster.Status();
+                Console.WriteLine("Choose an action:");
+                Console.WriteLine("1. Feed");
+                Console.WriteLine("2. Play");
+                Console.WriteLine("3. Sleep");
+                Console.WriteLine("4. Quit");
+
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+                Console.Write("\n");
+
+                switch(choice)
+                {
+                    case "1":
+                        monster.Feed();
+                        break;
+                    case "2":
+                        monster.Play();
+                        break;
+                    case "3":
+                        monster.Sleep();
+                        break;
+                    case "4":
+                        gameOver = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+
+                if(monster.Health <= 0)
+                {
+                    Console.WriteLine("Your pet is sick! Game Over.");
+                    gameOver = true;
+                }
+            }
+        }
+    }
+}
